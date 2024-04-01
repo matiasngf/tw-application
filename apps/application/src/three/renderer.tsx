4eb925d93bedfc2468e6e9389dbbafe93a8fa7be
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-03-31 23:42:15 -0300

    add antialias

--- apps/application/src/three/renderer.tsx
+++ apps/application/src/three/renderer.tsx
@@ -4,23 +4,31 @@ import { WebGLRenderTarget } from "three";
 import { RenderTexture } from "./components/render-texture";
 import { useMemo } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
-import { EffectComposer } from "three-stdlib";
+import { EffectComposer, FXAAShader, ShaderPass } from "three-stdlib";
 import { DrawPass } from "./shaders/draw-pass";
 import { InnerScene, useInnerScene } from "./inner-scene";
 
 export const Renderer = () => {
   const gl = useThree((s) => s.gl);
-  const mainFbo = useMemo(() => new WebGLRenderTarget(), []);
-
-  const innerCameraRef = useInnerScene((s) => s.cameraRef);
+  const mainFbo = useMemo(
+    () =>
+      new WebGLRenderTarget(1, 1, {
+        samples: 16,
+      }),
+    []
+  );
 
   const renderer = useMemo(() => {
     const composer = new EffectComposer(gl);
     composer.addPass(DrawPass);
     DrawPass.uniforms.baseTexture.value = mainFbo.texture;
 
+    const fxaaPass = new ShaderPass(FXAAShader);
+    composer.addPass(fxaaPass);
+
     return {
       composer,
+      fxaaPass,
     };
   }, [gl]);
 
@@ -28,13 +36,15 @@ export const Renderer = () => {
 
   const pixelRatio = useThree((s) => s.gl.getPixelRatio());
 
-  useFrame(({ gl }) => {
-    const { composer } = renderer;
-    const width = gl.domElement.width;
-    const height = gl.domElement.height;
+  useFrame(({ gl, size }) => {
+    const { composer, fxaaPass } = renderer;
 
+    DrawPass.uniforms.resolution.value = [size.width, size.height];
+    fxaaPass.uniforms.resolution.value.set(
+      (1 / size.width) * 0.3,
+      (1 / size.height) * 0.3
+    );
     composer.render();
-    DrawPass.uniforms.resolution.value = [width, height];
   }, 1);
 
   return (
