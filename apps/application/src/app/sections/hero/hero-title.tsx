b91c58373f650dd50dc2114e665a852c42f0870b
Matias Gonzalez Fernandez <matiasngf@hotmail.com>
2024-04-03 03:30:32 -0300

    add gh link

--- apps/application/src/app/sections/hero/hero-title.tsx
+++ apps/application/src/app/sections/hero/hero-title.tsx
@@ -5,6 +5,7 @@ import { Eye } from "./eye";
 import s from "./hero.module.css";
 import { useLenis } from "@studio-freight/react-lenis";
 import { useRef } from "react";
+import { GithubIcon } from "@/components/github";
 
 const name = "Matías González Fernández".split("");
 
@@ -19,38 +20,43 @@ export const HeroTitle = () => {
       titleRef.current &&
       headerContainerRef.current
     ) {
+      const headerBg =
+        headerContainerRef.current.querySelector<HTMLDivElement>(".header-bg");
+      if (!headerBg) return;
       if (l.scroll > containerRef.current.offsetTop) {
         titleRef.current.style.setProperty("position", "fixed");
-        headerContainerRef.current.style.setProperty("opacity", "1");
+        titleRef.current.style.setProperty("width", "initial");
+        headerBg.style.setProperty("opacity", "1");
       } else {
         titleRef.current.style.setProperty("position", "absolute");
-        headerContainerRef.current.style.setProperty("opacity", "0");
+        titleRef.current.style.removeProperty("width");
+        headerBg.style.setProperty("opacity", "0");
       }
     }
   });
 
   return (
-    <div ref={containerRef} className="relative text-title text-scale-3xl/7xl">
+    <div
+      ref={containerRef}
+      className="relative text-title text-scale-3xl/7xl flex flex-col items-center"
+    >
       <span aria-hidden className="hero-spacer whitespace-pre">
         {" "}
       </span>
       <header
         ref={headerContainerRef}
-        className="opacity-0 transition-opacity fixed top-0 z-[900] w-full h-[1.2em] border-b-foreground-300"
+        className="fixed top-0 z-[900] w-full h-[1.5em] md:h-[1.2em]"
       >
-        <div
-          className={clx(
-            "absolute inset-0 backdrop-blur-sm opacity-0",
-            s["header-mask"]
-          )}
-        ></div>
-        <div className="absolute inset-0 bg-gradient-to-t from-background/0 to-[2px] to-background"></div>
+        <div className="header-bg opacity-0 transition-opacity absolute inset-0 bg-gradient-to-t from-background/0 to-[2px] to-background"></div>
+        <div className="absolute inset-0 z-[1500]">
+          <GithubIcon className="absolute right-4 lg:right-8 top-1/2 -translate-y-1/2 w-6 lg:w-7" />
+        </div>
       </header>
       <div
         ref={titleRef}
-        className="h-[1.2em] top-0 absolute will-change-transform w-full z-[1000]"
+        className="h-[1.5em] md:h-[1.2em] top-0 absolute will-change-transform w-full z-[1000]"
       >
-        <h1 className="h-[1.2em] absolute top-1/2 -translate-y-1/2 overflow-hidden will-change-transform w-full text-scale-3xl/7xl z-[1000]">
+        <h1 className="h-[1.2em] relative top-1/2 -translate-y-1/2 overflow-hidden will-change-transform w-full text-scale-3xl/7xl z-[1000]">
           <span className="">
             {name.map((letter, index) => (
               <span
